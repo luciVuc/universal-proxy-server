8eabfe13fa84fac20b105694a824f144dd045ed9
Lucian <lucian.vuc@gmail.com>
2025-07-22 02:39:47 -0700

    fis: eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,46 +1,11 @@
-// // eslint.config.js
-// module.exports = {
-//   env: {
-//     es2021: true,
-//     node: true,
-//     jest: true,
-//   },
-//   parser: '@typescript-eslint/parser',
-//   parserOptions: {
-//     ecmaVersion: 'latest',
-//     sourceType: 'module',
-//   },
-//   plugins: ['@typescript-eslint', 'prettier'],
-//   extends: ['eslint:recommended',
-//     'plugin:@typescript-eslint/recommended',
-//     'plugin:prettier/recommended', // uses eslint-config-prettier + shows Prettier errors as lint errors
-//   ],
-//   rules: {
-//     'prettier/prettier': 'error',
-//   },
-//   ignorePatterns: ['node_modules/',
-//     'dist/',
-//     'coverage/',
-//     '*.config.js',
-//     '*.config.cjs',
-//     '*.config.mjs',
-//     '.eslintrc.*',
-//   ],
-// };
-
 // eslint.config.js
-import js from '@eslint/js';
+import '@eslint/js';
 import tseslint from '@typescript-eslint/eslint-plugin';
 import tsparser from '@typescript-eslint/parser';
 import prettier from 'eslint-config-prettier';
 import eslintPluginPrettier from 'eslint-plugin-prettier';
 
 export default [
-  // // JavaScript/Node base config
-  // js.config({
-  //   files: ['**/*.js', '**/*.cjs', '**/*.mjs'],
-  //   ignores: ['node_modules/', 'dist/', 'coverage/', '*.config.js'],
-  // }),
   // TypeScript config (for .ts and .tsx files)
   {
     files: ['src/**/*.ts', 'src/**/*.tsx'],
